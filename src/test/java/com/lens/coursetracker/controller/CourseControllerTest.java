e3a991509291c3bf899b8c732d40b0ad901013e9
lenshuygh <lens.huygh@gmail.com>
2018-06-13 21:42:50 +0200

    Changed CoursecontrollerTest for changed code in Coursecontroller & TagController, added tests for Validation Fail & Success on submitting a course changed methodNames to match other tests

--- src/test/java/com/lens/coursetracker/controller/CourseControllerTest.java
+++ src/test/java/com/lens/coursetracker/controller/CourseControllerTest.java
@@ -1,5 +1,6 @@
 package com.lens.coursetracker.controller;
 
+import com.lens.coursetracker.command.CourseCommand;
 import com.lens.coursetracker.converter.CourseCommandToCourse;
 import com.lens.coursetracker.service.CourseService;
 import com.lens.coursetracker.service.MyCourseService;
@@ -14,6 +15,10 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import javax.servlet.http.HttpSession;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -43,78 +48,86 @@ public class CourseControllerTest {
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
-        controller = new CourseController(courseService,tagService,myCourseService,session,courseCommandToCourse);
+        controller = new CourseController(courseService, tagService, myCourseService, session, courseCommandToCourse);
         mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
-
-
     }
 
     @Test
-    public void TestGetCourses() throws Exception{
+    public void getCourses() throws Exception {
         mockMvc.perform(get("/getCourses"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("course/courseOverview"))
                 .andExpect(model().attributeExists("courses"));
+        verify(courseService,times(1)).findAll();
+
     }
 
     @Test
-    public void TestShowFormStandard() throws Exception{
-        mockMvc.perform(get("/courseForm")
-                .sessionAttr("CreateTagFromCourseForm",false))
+    public void showFormStandard() throws Exception {
+        mockMvc.perform(get("/courseForm"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("course/courseForm"))
                 .andExpect(model().attributeExists("taglist"))
                 .andExpect(model().attributeExists("course"));
+        verify(tagService,times(1)).findAll();
 
     }
 
     @Test
-    public void TestShowFormAfterTagAdded() throws Exception{
-        mockMvc.perform(get("/courseForm")
-                .sessionAttr("CreateTagFromCourseForm",true))
-                .andExpect(status().isOk())
-                .andExpect(view().name("course/courseForm"))
-                .andDo(print())
-                .andExpect(model().attributeExists("taglist"));
-    }
-
-    @Test
-    public void TestSaveOrUpdateCourse() throws Exception{
+    public void saveOrUpdateCourse_SUCCESS() throws Exception {
         mockMvc.perform(post("/courseSubmit")
-                .param("submit","submit").
-                        contentType(MediaType.APPLICATION_FORM_URLENCODED))
-                .andExpect(status().isOk())
-                .andExpect(view().name("course/courseForm"))
-                .andExpect(model().attributeExists("taglist"));
-    }
-
-    @Test
-    public void TestAddTagInAddCourse() throws Exception {
-        mockMvc.perform(post("/courseSubmit")
-                .param("addtag","addtag"))
+                .param("submit", "submit")
+                .param("title","title")
+                .param("url","url"))
                 .andExpect(status().is3xxRedirection())
-                .andExpect(view().name("redirect:/tagForm"));
+                .andExpect(view().name("redirect:/getCourses"));
+        verify(courseService,times(1)).saveCourse(any(CourseCommand.class));
     }
 
     @Test
-    public void TestCancelCourseForm() throws Exception {
+    public void saveOrUpdateCourse_VALIDATION_FAIL() throws Exception {
         mockMvc.perform(post("/courseSubmit")
-        .param("cancel","cancel"))
+                .param("submit", "submit")
+                .param("title","")
+                .param("url","url"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("course/courseForm"))
+                .andExpect(model().attributeExists("taglist"))
+                .andExpect(flash().attributeCount(0));
+        verify(tagService,times(1)).findAll();
+    }
+
+    @Test
+    public void addTagInAddCourse() throws Exception {
+        mockMvc.perform(post("/courseSubmit")
+                .param("addtag", "addtag"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/tagFormFromCourse"))
+                .andExpect(flash().attributeExists("courseFormObject"))
+                .andExpect(flash().attributeExists("createTagFromCourseForm"));
+    }
+
+    @Test
+    public void cancelCourseForm() throws Exception {
+        mockMvc.perform(post("/courseSubmit")
+                .param("cancel", "cancel"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/getCourses"));
     }
 
     @Test
-    public void TestDeleteCourse() throws Exception{
+    public void deleteCourse() throws Exception {
         mockMvc.perform(get("/course/delete/1"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/getCourses"));
     }
 
     @Test
-    public void TestEditCourse() throws Exception{
+    public void editCourse() throws Exception {
         mockMvc.perform(get("/course/edit/1"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("course/courseForm"));
+        verify(courseService,times(1)).getCourse(anyInt());
+        verify(tagService,times(1)).findAll();
     }
 }
