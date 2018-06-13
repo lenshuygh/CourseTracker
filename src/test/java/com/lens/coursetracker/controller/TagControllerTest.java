fc35c5d768822559542bc796f8e86cbb04254b2f
lenshuygh <lens.huygh@gmail.com>
2018-06-13 22:52:46 +0200

    Changed TagControllerTest for changed code in Coursecontroller & TagController, added tests for Validation Fail & Success on submitting a Tag Organized imports on TagController

--- src/test/java/com/lens/coursetracker/controller/TagControllerTest.java
+++ src/test/java/com/lens/coursetracker/controller/TagControllerTest.java
@@ -1,5 +1,6 @@
 package com.lens.coursetracker.controller;
 
+import com.lens.coursetracker.command.CourseCommand;
 import com.lens.coursetracker.command.TagCommand;
 import com.lens.coursetracker.converter.CourseCommandToCourse;
 import com.lens.coursetracker.service.TagService;
@@ -7,29 +8,20 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpSession;
 
-import java.util.HashMap;
-
-import static org.junit.Assert.*;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 public class TagControllerTest {
 
@@ -49,76 +41,76 @@ public class TagControllerTest {
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
-        controller = new TagController(tagService,httpSession,courseCommandToCourse);
+        controller = new TagController(tagService, httpSession, courseCommandToCourse);
         mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
     }
 
     @Test
     public void getTags() throws Exception {
         mockMvc.perform(get("/getTags"))
+                .andExpect(status().isOk())
                 .andExpect(view().name("tag/tagOverview"))
                 .andExpect(model().attributeExists("tags"));
+        verify(tagService, times(1)).findAll();
     }
 
     @Test
-    public void showTagForm() throws Exception{
-        mockMvc.perform(get("/tagForm"))
+    public void showTagFormFromCourse() throws Exception {
+        mockMvc.perform(get("/tagFormFromCourse")
+                .param("courseFormObject", String.valueOf(new CourseCommand()))
+                .param("createTagFromCourseForm", "true"))
+                .andExpect(status().isOk())
                 .andExpect(view().name("tag/tagForm"))
                 .andExpect(model().attributeExists("tag"));
     }
 
     @Test
-    public void saveOrUpdateTag() throws Exception{
-        when(httpSession.getAttribute("CreateTagFromCourseForm")).thenReturn(true);
+    public void showTagForm() throws Exception {
+        mockMvc.perform(get("/tagForm"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("tag/tagForm"))
+                .andExpect(model().attributeExists("tag"));
+    }
 
+    @Test
+    public void saveOrUpdateTag_VALIDATION_SUCCESS() throws Exception {
         mockMvc.perform(post("/tagSubmit")
-                .param("cancel","")
-        )
+                .param("tagName","tagName"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/getTags"));
-
-
+        verify(tagService,times(1)).save(any(TagCommand.class));
     }
 
     @Test
-    public void saveOrUpdateTagValidationFailed() throws Exception{
-        //HashMap<String,Object> sessionAttributes = new HashMap<>();
-        //sessionAttributes.put("createTagFromCourseForm",false);
-        boolean createTagFromCourseForm = true;
-
+    public void saveOrUpdateTag_VALIDATION_ERROR() throws Exception{
         mockMvc.perform(post("/tagSubmit")
-                //.sessionAttrs(sessionAttributes)
-                        //.sessionAttr("CreateTagFromCourseForm",true)
-                //.contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                //.param("tagName","")
-                .param("cancel","cancel")
-                )
-                .andExpect(status().is3xxRedirection())
-                .andExpect(view().name("redirect:/getTags"))
-                .andDo(print());
-    }
-
-    @Test
-    public void deleteTag() throws Exception{
-        mockMvc.perform(get("/tag/delete/1"))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(view().name("redirect:/getTags"));
-    }
-
-    @Test
-    public void cancelTagForm() throws Exception{
-        mockMvc.perform(post("/tagSubmit")
-                .param("cancel","cancel"))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(view().name("redirect:/getTags"));
-
-
-    }
-
-    @Test
-    public void editTag() throws Exception{
-        mockMvc.perform(get("/tag/edit/1"))
+                .param("tagName",""))
                 .andExpect(status().isOk())
                 .andExpect(view().name("tag/tagForm"));
     }
+
+    @Test
+    public void deleteTag() throws Exception {
+        mockMvc.perform(get("/tag/delete/1"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/getTags"));
+        verify(tagService,times(1)).deleteById(anyInt());
+    }
+
+    @Test
+    public void cancelTagForm() throws Exception {
+        mockMvc.perform(post("/tagSubmit")
+                .param("cancel", "cancel"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/getTags"));
+    }
+
+    @Test
+    public void editTag() throws Exception {
+        mockMvc.perform(get("/tag/edit/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("tag/tagForm"));
+        verify(tagService,times(1)).getTag(anyInt());
+    }
+
 }
